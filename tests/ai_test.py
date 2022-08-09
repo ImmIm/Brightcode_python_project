ef44e56f41d34d24ad09a0ba1bc4e431133f4ebe
Kiryl Savich <kirillsavich90@gmail.com>
2022-08-09 06:12:55 +0300

    Small test fix v1.01

--- tests/ai_test.py
+++ tests/ai_test.py
@@ -6,11 +6,11 @@ from model.board import Board
 
 class DumbAiTest(unittest.TestCase):
     def setUp(self):
-        self.controller = ReverciController(8, '2','c', True, ('X', 'O'), 'c', 's', 2)
+        self.controller = ReverciController(8, '2','c', True, ('X', 'O'), 'c', 'd', 2)
         self.controller.game.rules.set_up_start()
    
     def test_make_move(self):
-        res = self.controller.game.current_player.make_move(self.controller.game.rules, self.controller.game.board, 1, 2, self.controller.show_moves_tips(self.controller.game))
+        res = self.controller.game.other_player.make_move(self.controller.game.rules, self.controller.game.board, 1, 2, self.controller.show_moves_tips(self.controller.game))
         self.assertIsInstance(res, tuple)
         self.assertIsInstance(res[0], tuple)
         self.assertIsInstance(res[0][0], int)
